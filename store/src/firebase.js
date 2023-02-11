1795176c313679e0fefb08fbf6c68b5592d5c4cf
Ehab Melhem <Melhemehab@gmail.com>
2023-02-11 21:24:16 +0200

    setup firestore

--- store/src/firebase.js
+++ store/src/firebase.js
@@ -2,6 +2,10 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
