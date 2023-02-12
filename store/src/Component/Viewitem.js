a851fb47fa5e107244981246e3ba893e55c27a1e
Ehab Melhem <Melhemehab@gmail.com>
2023-02-12 22:47:06 +0200

    add view item

--- store/src/Component/Viewitem.js
+++ store/src/Component/Viewitem.js
@@ -1,31 +1,47 @@
+import { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import { useLocation } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
+import { handleDelete } from "../firebase_handlers/handleDeleteItem";
+import { getProduct } from "../firebase_handlers/handleGetProduct";
 function Viewitem() {
   const location = useLocation();
+  let navigate = useNavigate();
+  const [data, setData] = useState(location.state.data);
+  const [id, setId] = useState(location.state.id);
 
   return (
-    <Card style={{ width: "18rem" }} className="card1">
-      <Card.Img
-        variant="top"
-        src="https://marvel-b1-cdn.bc0a.com/f00000000104050/www.monsterenergy.com/media/uploads_image/2022/09/28/1600/800/7bf4bcd3935de24cf53201a93c1f4e0c.jpg?mod=v1_40e763c91506132793778dcf5a0b7f92"
-      />
+    // <div>stam</div>
+    <Card style={{ width: "18rem", height: "500px" }} className="card1">
+      <Card.Img variant="top" src={data.imgUrl} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        <Card.Title>{data.name}</Card.Title>
       </Card.Body>
       <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+        <ListGroup.Item>Inventory: {data.inventory}</ListGroup.Item>
+        <ListGroup.Item>price: {data.price}</ListGroup.Item>
       </ListGroup>
       <Card.Body>
-        {/* i have to navigate to edit page with the item id */}
-        <Card.Link>Edit</Card.Link>
+        <Card.Link
+          onClick={() => {
+            console.log("delete");
+            handleDelete({ id: id });
+          }}
+        >
+          Delete
+        </Card.Link>
+        <Card.Link
+          onClick={() => {
+            navigate("/updateitem", {
+              state: {
+                id: id,
+              },
+            });
+          }}
+        >
+          update
+        </Card.Link>
         {/* <Card.Link href="#">Another Link</Card.Link> */}
       </Card.Body>
     </Card>
