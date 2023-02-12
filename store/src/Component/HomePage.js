b3fe3648eae5fc1a67f70a2a6270d1e66d70adb7
Ali Esa <aliesa059@gmail.com>
2023-02-12 22:18:42 +0200

    seeAllProduct

--- store/src/Component/HomePage.js
+++ store/src/Component/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row, Col, Image, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const styles = {
   imageContainer: {
@@ -13,21 +14,30 @@ const styles = {
   },
 };
 
-const HomePage = () => (
-  <div className="homepage__header">
-    <Container>
-      <Row className="align-items-center">
-        <Col xs={12}>
-          <h1>Welcome kamel store...</h1>
-        </Col>
-        <Col xs={12}>
-          <Button href="/products" variant="primary">
-            seeAllProducts
-          </Button>
-        </Col>
-      </Row>
-    </Container>
-  </div>
-);
+const HomePage = () => {
+  let navigate = useNavigate();
+
+  return (
+    <div className="homepage__header">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12}>
+            <h1>Welcome kamel store...</h1>
+          </Col>
+          <Col xs={12}>
+            <Button
+              onClick={() => {
+                navigate("/AllProduct");
+              }}
+              variant="primary"
+            >
+              seeAllProducts
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default HomePage;
